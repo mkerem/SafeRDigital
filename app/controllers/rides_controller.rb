0c14605c5f87e8c3b98d909cce7620aa71781c3b
Michaela Kerem <m.p.kerem@gmail.com>
2014-12-05 17:04:18 -0500

    More updates

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -4,19 +4,21 @@ def new
 end
 
 def create
-@post=Post.new(params[:post].permit(:title,:text))
-@post.save
-redirect_to @post
+
+render plain: params[:rides].inspect
+#@post=Post.new(params[:post].permit(:title,:text))
+#@post.save
+#redirect_to @post
 end
 
-private
-def post_params
-params.require(:post).permit(:title, :text)
-end
+#private
+#def post_params
+#params.require(:post).permit(:title, :text)
+#end
 
-public
-def show
-@post=Post.find(params:id])
-end
+#public
+#def show
+#@post=Post.find(params[:id])
+#end
 
 end
