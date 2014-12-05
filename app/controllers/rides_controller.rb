310236c9c0f955a78cf51596616df2e68dd69759
Michaela Kerem <m.p.kerem@gmail.com>
2014-12-05 05:24:23 -0500

    Adjusting to new repo

---
+++ app/controllers/rides_controller.rb
@@ -0,0 +1,22 @@
+class RidesController < ApplicationController
+
+def new
+end
+
+def create
+@post=Post.new(params[:post].permit(:title,:text))
+@post.save
+redirect_to @post
+end
+
+private
+def post_params
+params.require(:post).permit(:title, :text)
+end
+
+public
+def show
+@post=Post.find(params:id])
+end
+
+end
